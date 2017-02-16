6987c9bf8d966bedf37f5e58689b07545214096b
Al-Juwan Williams <syntheticizayoi@gmail.com>
2017-02-16 11:33:44 -0500

    DID PERFECT FUCK YOU PERFECT

--- CS_Chapter4/Perfect/Program.cs
+++ CS_Chapter4/Perfect/Program.cs
@@ -10,7 +10,20 @@ namespace Perfect
     {
         static void Main(string[] args)
         {
-            
+            int num = 1000, count = 0;
+            for (int i = 1; i<num;i++)
+            {
+                count = 0;
+                for (int p = 1; p < i; p++)
+                {
+                    if (i%p == 0)
+                    {
+                        count = count + p;
+                    }
+                }
+                if (count == i)
+                    Console.WriteLine(i);
+            }
         }
     }
 }
--- CS_Chapter4/TubSales/Program.cs
+++ CS_Chapter4/TubSales/Program.cs
@@ -10,6 +10,17 @@ namespace TubSales
     {
         static void Main(string[] args)
         {
+            String sp = "";
+            Console.WriteLine("Pick a salesperson: Andrea, Brittany, or Eric");
+            while (sp.ToLower() != "z")
+            {
+                sp = Console.ReadLine();
+                if (sp.ToLower().StartsWith("a"))
+                {
+                    Console.WriteLine("What was the pay")
+                }
+            }
+
         }
     }
 }
