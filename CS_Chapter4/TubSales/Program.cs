eee976d9464993cfe17a3fe27aef9d567bb52ddf
Al-Juwan Williams <syntheticizayoi@gmail.com>
2017-02-16 19:20:11 -0500

    added $ to tub sales

--- CS_Chapter4/TubSales/Program.cs
+++ CS_Chapter4/TubSales/Program.cs
@@ -11,16 +11,30 @@ namespace TubSales
         static void Main(string[] args)
         {
             String sp = "";
-            Console.WriteLine("Pick a salesperson: Andrea, Brittany, or Eric");
+            double apay = 0, bpay = 0, epay = 0;
             while (sp.ToLower() != "z")
             {
+                Console.WriteLine("Pick a salesperson: Andrea, Brittany, or Eric");
                 sp = Console.ReadLine();
                 if (sp.ToLower().StartsWith("a"))
                 {
-                    Console.WriteLine("What was the pay")
+                    Console.WriteLine("What were the tubs Andrea sold worth: ");
+                    apay += Convert.ToDouble(Console.ReadLine());
                 }
-            }
+                else if (sp.ToLower().StartsWith("b"))
+                {
+                    Console.WriteLine("What were the tubs Brittany sold worth: ");
+                    bpay += Convert.ToDouble(Console.ReadLine());
+                }
+                else if (sp.ToLower().StartsWith("e"))
+                {
+                    Console.WriteLine("What were the tubs Eric sold worth: ");
+                    epay += Convert.ToDouble(Console.ReadLine());
+                }
 
+            }
+            double acom = apay * .10, bcom = bpay * .10, ecom = epay * .10;
+            Console.WriteLine("\t\tSales\t\tCommission\nAndrea\t\t" + apay.ToString("C") + "\t\t" + acom.ToString("C") + "\nBrittany\t" + bpay.ToString("C") + "\t\t" + bcom.ToString("C") + "\nEric\t\t" + epay.ToString("C") + "\t\t" + ecom.ToString("C"));
         }
     }
 }
